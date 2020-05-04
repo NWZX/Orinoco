75c0c915c0ef686d6de2c07d335756d8d57473b1
NWZX <nwzx@users.noreply.github.com>
2020-05-04 11:56:31 -0400

    Add price update methode

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ function scrap_price(price) {
     price /= 1000;
     return price;
 }
+function UR_price(response) {
+    let result = 0;
+    let card = localStorage.getItem("card").split(",");
+    for (let [key, value] of Counter(card)) {
+        const ele = response.find(n => n._id == key);;
+        result += value * scrap_price(ele.price);
+    }
+    let summary = document.getElementsByClassName('summary-table');
+    summary[0].innerHTML = '<li><span>subtotal:</span> <span>$' + result.toFixed(2) + '</span></li>' +
+        '<li><span>delivery:</span> <span>Free</span></li>' +
+        '<li><span>total:</span> <span>$' + result.toFixed(2) + '</span></li>';
+}
