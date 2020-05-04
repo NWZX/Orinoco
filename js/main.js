c74b0d5d6fdf71c834a166c0c24db36e8c4c0f15
NWZX <nwzx@users.noreply.github.com>
2020-05-04 11:55:57 -0400

    Add scrap_price method

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 const regex_email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const regex_name = /\d/;
 const server = "http://207.180.251.133:4000";
+
+function scrap_price(price) {
+    price /= 1000;
+    return price;
+}
